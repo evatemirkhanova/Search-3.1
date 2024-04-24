17a58e14c23ee468f92db807cd58e18fc4ea046b
Ева <145904841+evatemirkhanova@users.noreply.github.com>
2024-04-24 13:07:14 +0500

    Add files via upload

---
+++ l3-proba/BinarySearchCount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l3_proba
+{
+    internal class BinarySearchCount
+    {
+        // Метод для оптимизированного поиска числа в отсортированном массиве
+        public static int BSC(int[] array, int number)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            int mid;
+            int count = 0;
+
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+
+                // Если число найдено, ищем сколько элементов равны ему
+                if (array[mid] == number)
+                {
+                    count++;
+                    // Проверяем элементы слева от найденного
+                    int leftIndex = mid - 1;
+                    while (leftIndex >= 0 && array[leftIndex] == number)
+                    {
+                        count++;
+                        leftIndex--;
+                    }
+                    // Проверяем элементы справа от найденного
+                    int rightIndex = mid + 1;
+                    while (rightIndex < array.Length && array[rightIndex] == number)
+                    {
+                        count++;
+                        rightIndex++;
+                    }
+                    return count;
+                }
+                // Если искомое число меньше, идем в левую часть массива
+                else if (array[mid] > number)
+                {
+                    right = mid - 1;
+                }
+                // Если искомое число больше, идем в правую часть массива
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            // Если число не найдено
+            return count;
+        }
+    }
+}
---
+++ l3-proba/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l3_proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arrayLength];
+
+            Random random = new Random();
+
+            // Заполняем массив случайными числами и сразу сортируем его
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            
+            // Создаем объект класса InsertionSort
+            InsertionSort sorter = new InsertionSort();
+
+            // Сортируем массив с использованием сортировки вставками
+            sorter.Ins_Sort(array);
+
+            // Можем вывести отстортированный массив, если захотим:
+            Console.WriteLine("Отсортированный массив: " + string.Join(", ", array));
+
+            Console.WriteLine("Введите число X для поиска в массиве: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Вызываем метод для поиска числа в массиве
+            int count = BinarySearchCount.BSC(array, number);
+
+            Console.WriteLine("Сколько элементов равны X? - " + count);
+        }
+    }
+}
